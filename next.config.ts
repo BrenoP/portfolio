4072a58e65bf3d2270b3f6534f5675f87e3daa55
Breno Peretta <peretta.breno@gmail.com>
2024-03-06 13:22:00 -0300

    Ajuste no module do next

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-};
+// const nextConfig: NextConfig = {
+//   output: 'export',
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true
+//   },
+//   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+const isGithubPages = process.env.NODE_ENV === 'production'
+
+const repoName = 'portifolio-pessoal' // nome do repositório no GitHub
+
+module.exports = {
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  trailingSlash: true, // importante para funcionar bem no GitHub Pages
+}
