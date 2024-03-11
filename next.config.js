009775121fb82d4934d2417320d64ffa7b5b3f32
Breno Peretta <peretta.breno@gmail.com>
2024-03-11 13:22:00 -0300

    next.config em js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const repo = 'portfolio';
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+};
+
+module.exports = nextConfig;
